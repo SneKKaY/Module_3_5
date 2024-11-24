20f0bf4b2a8732939dadd1bcbe3f7067ee4206f1
SneKKaY <120265382+SneKKaY@users.noreply.github.com>
2024-11-24 11:33:33 +0300

    Add files via upload

---
+++ Module_3_5.py
@@ -0,0 +1,23 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    if first == 0:
+        return 1
+    else:
+        return first
+
+
+
+
+
+
+result = get_multiplied_digits(40203)
+
+print(result)
+
+result2 = get_multiplied_digits(4020300000000000000000000000000000000000000000000000000000000000000000000000000000000)
+
+print(result2)
